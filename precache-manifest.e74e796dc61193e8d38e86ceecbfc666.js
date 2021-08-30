fd5eb960da61ce11ccb767a9717f5f70ca0888b2
santhosh boya <santhosh.boya@ibhubs.co.in>
2021-08-30 22:48:24 +0530

    Updates

---
+++ precache-manifest.e74e796dc61193e8d38e86ceecbfc666.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e27c9f47d1eff5a15c2774cca1cd00f",
+    "url": "/Reporting-Portal/index.html"
+  },
+  {
+    "revision": "dc5d859280f8d082833a",
+    "url": "/Reporting-Portal/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "041e43ade9663a6ab3cc",
+    "url": "/Reporting-Portal/static/css/main.7feda122.chunk.css"
+  },
+  {
+    "revision": "dc5d859280f8d082833a",
+    "url": "/Reporting-Portal/static/js/2.51229e18.chunk.js"
+  },
+  {
+    "revision": "3e6cde9637a314d42e0882033276ed79",
+    "url": "/Reporting-Portal/static/js/2.51229e18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "041e43ade9663a6ab3cc",
+    "url": "/Reporting-Portal/static/js/main.949014b0.chunk.js"
+  },
+  {
+    "revision": "e2c1018513346b389814",
+    "url": "/Reporting-Portal/static/js/runtime-main.7f55f481.js"
+  }
+]);
